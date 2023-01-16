089be5d15f85823ab850288ca996097cfab78943
William Ortiz <william.ortiz@mvm.com.co>
2023-01-16 15:43:47 -0500

    Fusion Carrito de compras

--- main.js
+++ main.js
@@ -2,15 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
+
+    if(!isCarritoAsideClosed){
+            aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
+
+    if(!isCarritoAsideClosed){
+            aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactvie');
+    const isDesktopClosed = desktopMenu.classList.contains('inactvie');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');    
+    }
+    if(!isDesktopClosed){
+        desktopMenu.classList.add('inactive');    
+    }
+
+    aside.classList.toggle('inactive');
 }
